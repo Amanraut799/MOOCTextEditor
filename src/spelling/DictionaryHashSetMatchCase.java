cc219599bcefcbc493d3c4c1df28a09e016e7314
Amanraut799 <101193234+Amanraut799@users.noreply.github.com>
2022-09-16 01:06:41 +0530

    Add files via upload

---
+++ src/spelling/DictionaryHashSetMatchCase.java
@@ -0,0 +1,23 @@
+package spelling;
+
+public class DictionaryHashSetMatchCase implements Dictionary {
+
+	@Override
+	public boolean addWord(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isWord(String s) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
